5bc89cf1e498db34e71185467fb40d0168fefb55
Brian Stump <brian.stump@brianstump-ltm.internal.salesforce.com>
2023-01-23 14:54:11 -0500

    create files

---
+++ countries.py
@@ -0,0 +1,37 @@
+import csv
+import pandas as pd
+import os
+import snowflake.connector
+
+c = snowflake.connector.connect(
+    user=os.environ['snowflakeuser'],
+    password=os.environ['snowflakepass'],
+    account='your-snowflake-account-identifier-here',
+    warehouse='your-warehouse-name',
+    role='your-role-name'
+    database='your-database-name',
+    schema ='your-default-schema-name'
+    )
+c = conn.cursor()
+
+c.execute('DROP TABLE IF EXISTS countries;')
+
+c.execute('''
+    CREATE TABLE countries (
+    name TEXT NOT NULL ,
+    alpha TEXT NOT NULL ,
+    code INTEGER NOT NULL ,
+    region TEXT NOT NULL ,
+    intermediate_region TEXT,
+    PRIMARY KEY (code)
+    );
+    ''')
+
+with open('countries.csv','r') as fin:
+    dr = csv.DictReader(fin) 
+    to_db = [(i['name'], i['alpha-3'], i['country-code'], i['region'], i['intermediate-region']) for i in dr]
+
+c.executemany("INSERT INTO countries (name, alpha, code, region, intermediate_region) VALUES (?, ?, ?, ?, ?);", to_db)
+
+conn.commit()
+c.close()
